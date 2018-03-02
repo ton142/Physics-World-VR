7c306fbed24d475493ffda95b7675948e474feef
treysfordays <austinkwok@live.ca>
2018-03-01 18:03:22 -0800

    updated ui script

---
+++ PhysicsWorldX/Assets/UIText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour {
+
+    [SerializeField]
+    private Text projectileInfo = null;
+
+    public Slingshot slingshot;
+
+	// Use this for initialization
+	void Start () {
+        projectileInfo.text = "Velocity: " + slingshot.GetComponent<Slingshot>().getShootVelocity();
+	}
+	
+}
